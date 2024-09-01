575cf5d1317ad3f8a9f0f617542375296db792b8
jonasbeltoft <jonasb5@hotmail.dk>
2024-09-01 05:49:11 +0200

    vue init

---
+++ umbraco-app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { onMounted } from "vue";
+
+function handleClick() {
+  document.title = "hello there";
+}
+
+onMounted(() => {
+  document.title = "Beginning";
+});
+</script>
+<template>
+  <div>
+    <h1>Bubba</h1>
+    <Button @click="handleClick()">Hello there</Button>
+  </div>
+</template>
