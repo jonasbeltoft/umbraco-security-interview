9f3b1544575e871782f6e37ea88ffa4773b37c82
jonasbeltoft <jonasb5@hotmail.dk>
2024-09-03 05:07:43 +0200

    Dragging implemented

--- umbraco-app/src/App.vue
+++ umbraco-app/src/App.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-import { Button } from "@/components/ui/button";
-import { onMounted } from "vue";
+import DragList from "./DragList.vue";
 
-function handleClick() {
-  document.title = "hello there";
-}
-
-onMounted(() => {
-  document.title = "Beginning";
-});
+let description = "This is a description";
 </script>
+
 <template>
-  <div>
-    <h1>Bubba</h1>
-    <Button @click="handleClick()">Hello there</Button>
-  </div>
+  <main class="w-full max-w-5xl min-w-[648px] mx-auto flex flex-col pt-4 gap-2">
+    <h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
+      {{ description }}
+    </h4>
+    <DragList />
+  </main>
 </template>
+
+<style></style>
---
+++ umbraco-app/src/DragList.vue
@@ -0,0 +1,73 @@
+<script>
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardDescription,
+} from "./components/ui/card";
+
+import draggable from "vuedraggable";
+
+let id = 1;
+export default {
+  name: "simple",
+  display: "Simple",
+  order: 0,
+  components: {
+    draggable,
+    Card,
+    CardTitle,
+    CardHeader,
+    CardDescription,
+  },
+  data() {
+    return {
+      enabled: true,
+      list: [
+        {
+          name: "English",
+          code: "en",
+        },
+        {
+          name: "Spanish",
+          code: "es",
+        },
+        {
+          name: "French",
+          code: "fr",
+        },
+      ],
+      dragging: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <draggable
+    :list="list"
+    item-key="code"
+    class="rounded-md border p-6 flex flex-col gap-3"
+    ghost-class="ghost"
+    :move="checkMove"
+    @start="dragging = true"
+    Frenchfr
+    @end="dragging = false"
+  >
+    <template #item="{ element: language }">
+      <Card class="transition-all cursor-move">
+        <CardHeader>
+          <CardTitle>{{ language.name }}</CardTitle>
+          <CardDescription>{{ language.code }}</CardDescription>
+        </CardHeader>
+      </Card>
+    </template>
+  </draggable>
+</template>
+
+<style scoped>
+.ghost {
+  opacity: 1;
+  @apply bg-accent;
+}
+</style>
