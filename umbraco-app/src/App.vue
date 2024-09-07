4a09b44b96d9f32ba6d126dca9d95a36a51de068
jonasbeltoft <jonasb5@hotmail.dk>
2024-09-08 01:54:37 +0200

    de-compositioned list for live storage ref

--- umbraco-app/src/App.vue
+++ umbraco-app/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import DragList from "./DragList.vue";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -16,6 +15,17 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Search } from "lucide-vue-next";
 import { Input } from "@/components/ui/input";
 import { useStorage } from "@vueuse/core";
+import { Trash } from "lucide-vue-next";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import draggable from "vuedraggable";
 
 const page_text = useStorage("page_text", {
   button_text: "Tilføg sprog",
@@ -140,13 +150,10 @@ let all_languages = ref([
   },
 ]);
 
+const DEFAULT_LANGUAGES = ["da", "en"];
+
 // Load chosen languages from local storage
-const chosen_languages = useStorage(
-  "chosen_languages",
-  all_languages.value
-    .filter((lang) => lang.chosen)
-    .map((lang) => lang.current_locale)
-);
+const chosen_languages = useStorage("chosen_languages", DEFAULT_LANGUAGES);
 
 let search = ref("");
 
@@ -160,6 +167,25 @@ function onlanguagechange(lang: string) {
 function addLanguages() {
   console.log("Adding languages");
 }
+
+function updateLocale(locale: string) {
+  all_languages.value.forEach((lang) => {
+    if (lang.locales.includes(locale)) {
+      lang.current_locale = locale;
+    }
+  });
+}
+
+function deleteLanguage(language: any) {
+  if (chosen_languages.value.length === 1) {
+    chosen_languages.value = DEFAULT_LANGUAGES.flat();
+    return;
+  }
+  const index = chosen_languages.value.indexOf(language);
+  chosen_languages.value.splice(index, 1);
+}
+
+const getItemKey = (element: string, index: number) => `${element}-${index}`;
 </script>
 
 <template>
@@ -169,11 +195,55 @@ function addLanguages() {
     <h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
       {{ page_text.description }}
     </h4>
-    <DragList
-      v-bind:current_languages="chosen_languages"
-      v-bind:all_languages="all_languages"
-      @lang_change="onlanguagechange"
-    />
+    <draggable
+      :list="chosen_languages"
+      :itemKey="getItemKey"
+      class="rounded-md border p-6 flex flex-col gap-3"
+      ghost-class="ghost"
+    >
+      <template #item="{ element: lang_code }">
+        <Card class="transition-all cursor-move flex flex-row">
+          <CardHeader>
+            <CardTitle>{{
+              all_languages.find((lang) => lang.locales.includes(lang_code))
+                ?.name
+            }}</CardTitle>
+          </CardHeader>
+          <Select
+            :default-value="
+              all_languages.find((lang) => lang.locales.includes(lang_code))
+                ?.current_locale
+            "
+            v-on:update:model-value="updateLocale"
+          >
+            <SelectTrigger class="max-w-xs ml-auto my-auto mr-3">
+              <SelectValue placeholder="Select a language" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem
+                  v-for="(locale, i) in all_languages.find((lang) =>
+                    lang.locales.includes(lang_code)
+                  )?.locales"
+                  :value="locale"
+                  :key="i"
+                >
+                  {{ locale }}
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Button
+            @click="deleteLanguage(lang_code)"
+            class="my-auto mr-4"
+            variant="outline"
+            size="icon"
+          >
+            <Trash class="w-4 h-4" />
+          </Button>
+        </Card>
+      </template>
+    </draggable>
     <!-- MARK: Add Languages Dialog / Modal -->
     <Dialog>
       <!-- v-on:update:open="(open) => (open ? '' : (search = ''))" -->
@@ -241,4 +311,9 @@ function addLanguages() {
   </main>
 </template>
 
-<style></style>
+<style scoped>
+.ghost {
+  opacity: 1;
+  @apply bg-accent;
+}
+</style>
